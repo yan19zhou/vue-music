5ed0daf3c551e76f6f91c20093867e72e9a81945
zhouyan <1360982972@qq.com>
2018-07-24 15:21:21 +0800

    1

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="recommend" ref="recommend">
-      <div>
+      <scroll ref="scroll" class="recommend-content" :data="discList">
+        <div >
         <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">  
             <slider v-if="recommends">                
                   <div v-for="item in recommends">
                     <a :href="item.linkUrl">
-                    <img :src="item.picUrl" alt="">
+                    <img @load="imageLoad" :src="item.picUrl" alt="">
                     </a>
                   </div>              
             </slider>
@@ -15,7 +16,7 @@
           <ul>
             <li v-for="item in discList"  class="item" :unicode="item.dissid">
               <div class="icon">
-                <img :src="item.imgurl" alt="" width="120px" height="120px">
+                <img :src="item.imgurl" alt="" width="80px" height="80px">
               </div>
               <div class="text">
                 <h2 class="name">{{item.creator.name}}</h2>
@@ -24,20 +25,22 @@
             </li>
           </ul>
         </div>
-      </div>
+        </div>
+      </scroll>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import Slider from 'base/slider/slider'
-   import {getRecommend, getDiscList} from 'api/recommend'
+  import Scroll from 'base/scroll/scroll'
+  import {getRecommend, getDiscList} from 'api/recommend'
   import {ERR_OK} from 'api/config'
-  //import Scroll from "base/scroll/scroll";
+ 
 
   export default {
     components:{
       Slider,
-      
+      Scroll
     },
     data() {
       return {
@@ -46,7 +49,7 @@
       }
     },
     created() {
-      this._getRecommend()
+      this._getRecommend()  
       this._getDiscList()
     },
     methods: {
@@ -61,10 +64,16 @@
         getDiscList().then((res) => {
           if (res.code === ERR_OK) {
             this.discList = res.data.list
-            console.log(res.data)
+           // console.log(res.data)
           }
         })
       },
+      imageLoad(){
+        if (!this.checkLoad) {
+          this.$refs.scroll.refresh()
+          this.checkLoad = true
+        }
+      }
       
     }   
   }
@@ -73,11 +82,11 @@
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
 
-  .recommend
-    position: fixed
-    width: 100%
-    top: 88px
-    bottom: 0
+    .recommend
+      position: fixed
+      width: 100%
+      top: 88px
+      bottom: 0
     .recommend-content
       height: 100%
       overflow: hidden
@@ -86,8 +95,37 @@
         width: 100%
         overflow: hidden
       .recommend-list
+        .list-title
+          height: 65px
+          line-height: 65px
+          text-align: center
+          font-size: $font-size-medium
+          color: $color-theme
         .item
-            display flex 
-            
-
+          display: flex
+          box-sizing: border-box
+          align-items: center
+          padding: 0 20px 20px 20px
+          .icon
+            flex: 0 0 80px
+            width: 80px
+            padding-right: 20px
+          .text
+            display: flex
+            flex-direction: column
+            justify-content: center
+            flex: 1
+            line-height: 20px
+            overflow: hidden
+            font-size: $font-size-medium
+            .name
+              margin-bottom: 10px
+              color: $color-text
+            .desc
+              color: $color-text-d
+      .loading-container
+        position: absolute
+        width: 100%
+        top: 50%
+        transform: translateY(-50%)
 </style>
