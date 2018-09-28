1f4e6e56ec7e415b6c5ed1e2488e8f19154fa19c
zhouyan <1360982972@qq.com>
2018-09-28 14:23:29 +0800

    33

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,43 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Recommend from 'components/recommend/recommend'
+/* import Recommend from 'components/recommend/recommend'
 import Rank from 'components/rank/rank'
 import Search from 'components/search/search'
 import Singer from 'components/singer/singer'
 import SingerDetail from 'components/singer-detail/singer-detail'
-
+ */
 Vue.use(Router)
+
+const Recommend = ((resolve)=>{
+    import('components/recommend/recommend').then((module)=>{
+        resolve(module)
+    }) 
+})
+
+const Rank = ((resolve)=>{
+    import('components/rank/rank').then((module)=>{
+        resolve(module)
+    }) 
+})
+
+const Search = ((resolve)=>{
+    import('components/search/search').then((module)=>{
+        resolve(module)
+    }) 
+})
+
+const Singer = ((resolve)=>{
+    import('components/singer/singer').then((module)=>{
+        resolve(module)
+    }) 
+})
+
+const SingerDetail = ((resolve)=>{
+    import('components/singer-detail/singer-detail').then((module)=>{
+        resolve(module)
+    }) 
+})
+
 
 export default new Router({
   routes:[
