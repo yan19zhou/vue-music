5548302a87e127c945b6e1101a86bdd7e52017d5
zhouyan <1360982972@qq.com>
2018-09-29 15:50:35 +0800

    0.0

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-/* import Recommend from 'components/recommend/recommend'
-import Rank from 'components/rank/rank'
-import Search from 'components/search/search'
-import Singer from 'components/singer/singer'
-import SingerDetail from 'components/singer-detail/singer-detail'
- */
+import Recommend from 'components/recommend/recommend'
+
 Vue.use(Router)
 
-const Recommend = ((resolve)=>{
+/* const Recommend = ((resolve)=>{
     import('components/recommend/recommend').then((module)=>{
         resolve(module)
     }) 
-})
+}) */
 
 const Rank = ((resolve)=>{
     import('components/rank/rank').then((module)=>{
@@ -38,6 +34,11 @@ const SingerDetail = ((resolve)=>{
     }) 
 })
 
+const Disc =((resolve)=>{
+    import('components/disc/disc').then((module)=>{
+        resolve(module)
+    })
+})
 
 export default new Router({
   routes:[
@@ -47,7 +48,13 @@ export default new Router({
       },
       {
           path:'/recommend',
-          component: Recommend
+          component: Recommend,
+          children: [
+            {
+              path: ':id',
+              component: Disc
+            }
+          ]
       },
       {
         path:'/rank',
